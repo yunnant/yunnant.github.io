533a46946c2052a76d1bfe33aa4ce6597934d807
leonhuxf <leonhuxf@umich.edu>
2018-05-05 03:58:41 -0400

    xxx

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         target: 'instafeed',
         clientId: '8197ce5e132f4ad6aa1ad85ad821f6ea',
         accessToken: '977617522.8197ce5.f3f18d80860b48a5a523f82de0921a05',
-        limit: 12,
+        limit: 1,
         resolution: 'low_resolution',
         sortBy: 'most-liked',
         template: '<div class="gallery-item"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
