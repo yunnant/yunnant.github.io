cd5af6effd03d8ca58e2dfcd29d2e2427b8b06db
leonhuxf <leonhuxf@umich.edu>
2018-05-10 16:57:47 -0400

    instagram fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,26 +7,40 @@ $(document).ready(function() {
         target: 'instafeed',
         clientId: '8197ce5e132f4ad6aa1ad85ad821f6ea',
         accessToken: '977617522.8197ce5.f3f18d80860b48a5a523f82de0921a05',
-        limit: 1,
-        resolution: 'low_resolution',
-        sortBy: 'most-liked',
-        template: '<div class="gallery-item"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        limit: 15,
+        resolution: 'standard_resolution',
+        sortBy: 'most-recent',
+        template: '<article class="6u 12u$(xsmall) work-item"><a href="{{image}}" class="image fit thumb"><img src="{{image}}" alt="{{image}}" /></a><h3><a href={{link}} target="_blank"> Posted on {{model.created_time}}</a> </h3><p>{{caption}} @ {{location}}</p></article>',
+        filter: function(image) {
+
+        var date = new Date(image.created_time*1000);
+
+        m = date.getMonth();
+        d = date.getDate();
+        y = date.getFullYear();
+
+        var month_names = new Array ( );
+        month_names[month_names.length] = "Jan";
+        month_names[month_names.length] = "Feb";
+        month_names[month_names.length] = "Mar";
+        month_names[month_names.length] = "Apr";
+        month_names[month_names.length] = "May";
+        month_names[month_names.length] = "Jun";
+        month_names[month_names.length] = "Jul";
+        month_names[month_names.length] = "Aug";
+        month_names[month_names.length] = "Sep";
+        month_names[month_names.length] = "Oct";
+        month_names[month_names.length] = "Nov";
+        month_names[month_names.length] = "Dec";
+
+        var thetime = month_names[m] + ' ' + d + ' ' + y;
+
+        image.created_time = thetime;
+
+        return true;
+    }
     });
 
 
     userFeed.run();
-
-    
-    // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
-
-
-
-
 });
